aeab73e2c75fb0bad29d1b2e92bdec4c4ca31f68
Victor Bodell <vbodell@kth.se>
2017-09-21 21:48:47 -0700

    wrote free, calloc & malloc, nothing tested...

--- myalloc.c
+++ myalloc.c
@@ -12,140 +12,22 @@ First working version: TBA
 #include <stdint.h>
 #include <errno.h> /* For error handling, including EXIT_FAILURE */
 #include <unistd.h> /* For syscall declarations */
-
-/* DATA STRUCTURE FOR MEMORY CHUNKS
-size: contains sizeof memory-map (exluding sizeof chunk)
-isfree: is chunk of memory free to use?
-memptr: pointer to actual memory space
-next: pointer to next chunk header */
-struct chunk{
-  uintptr_t chunksize;
-  char isfree;
-  uintptr_t memptr;
-  struct chunk *next;
-};
-
-#define ALIGNER 16
-
-/* Roundup to next multiple of 16 if size of chunk is not already */
-static int roundup = ALIGNER -
-        ((sizeof(struct chunk) % ALIGNER) ?
-              sizeof(struct chunk) % ALIGNER : ALIGNER);
-/*Define size of chunk header*/
-#define HEADERSIZE (sizeof(struct chunk) + roundup)
-
-//declare memory buffer, HOW TO initialize??
-static struct chunk *MEM;
-
-/*Lowest break*/
-static uintptr_t BREAK = 0;
-
-/*Upper Limit before we need sbrk*/
-static uintptr_t UPPERLIM;
-
-/* A big piece is a CAKE, which we turn into chunks (Pizza-slice?)*/
-#define CAKESIZE 1024
-
-/*Define sbrk(2) error-value*/
-#define SBRKERR (struct chunk*)-1
-
-/*To improve readability*/
-#define TRUE 1
-#define FALSE 0
-
-
-
-void *more_memory_please(struct chunk *node, size_t sizerequest){
-  struct chunk *newbreak;
-  if((newbreak = (struct chunk*) sbrk(CAKESIZE)) == SBRKERR){
-    /*sbrk(2) failed*/
-    errno = ENOMEM;
-    return NULL;
-  }
-
-  /*If First BREAK has not been set, set it*/
-  if(!BREAK)
-    BREAK = (uintptr_t) newbreak;
-
-  UPPERLIM = (uintptr_t) newbreak + CAKESIZE;
-  /*current address for user memory & header (only offset is header)*/
-  uintptr_t current_user_memory = (uintptr_t) newbreak+HEADERSIZE;
-  uintptr_t current_header = (uintptr_t) newbreak;
-
-  /*Correctly initialize starting chunk to correct place in memory*/
-  newbreak->chunksize = sizerequest,
-  newbreak->isfree = FALSE,
-  newbreak->memptr = current_user_memory;
-
-
-  /*If node is NULL, there is no root & memory has not been initialized*/
-  if(node == NULL){
-    MEM = newbreak;
-  }
-  else{ /*We have traversed linked list already*/
-    node->next = newbreak;
-  }
-
-  /* node used for traverse*/
-  struct chunk *temp = newbreak;
-
-  /*set memory pointers to next position*/
-  uintptr_t chunk_tot_sz = sizerequest + HEADERSIZE;
-  current_user_memory += chunk_tot_sz;
-  current_header += chunk_tot_sz;
-
-  int i;
-  /*HOW TO ASSIGN CHUNKS?? IMPROVE FORMULA!!*/
-  for(i = 1; (current_user_memory+ALIGNER*i+HEADERSIZE) < UPPERLIM; i++){
-    i = (i%129); /*Maximum chunksize will be 128*16=2048 bytes*/
-    int thissize = ALIGNER*i;
-    chunk_tot_sz = HEADERSIZE + thissize;
-
-    /*Set address for next, and move to it for initialization*/
-    temp->next = (struct chunk*) current_header;
-    temp = temp->next;
-    /*move to next header*/
-    current_header += chunk_tot_sz;
-
-    /*Give chunk memorysize, set free=TRUE, pointer=current_user_memory+header+sizerequest, */
-    temp->chunksize = thissize,
-    temp->isfree = TRUE,
-    temp->memptr = current_user_memory;
-
-    /*Increment current_user_memory to next address for user-memory*/
-    current_user_memory += chunk_tot_sz;
-  }
-
-
-  uintptr_t remaining_CAKE = UPPERLIM - current_user_memory;
-  if(remaining_CAKE){/*Create chunk out of remaining memoryspace*/
-    temp->next = (struct chunk*) current_header;
-    temp = temp->next;
-
-    temp->chunksize = remaining_CAKE;
-    temp->isfree = TRUE;
-    temp->memptr = current_user_memory;
-
-    /*no need to increment traverse variables since we are done*/
-  }
-
-
-  return (void*) newbreak->memptr;
-}
+#include "myalloc.h"
 
 
 
 
 /*Okay, so now this should return some memory, but sbrk2 should probably
 be modified to correctly address where headers are put...*/
-void *malloc(size_t sizerequest){
+void *myalloc(size_t sizerequest){
   if(sizerequest <= 0){
     /*Nothing to allocate*/
     return NULL;
   }
+
   /* round size to closest multiple of 16 */
-  if(sizerequest % 16){
-    sizerequest = sizerequest + 16 - (sizerequest % 16);
+  if(sizerequest % ALIGNER){
+    sizerequest = sizerequest + ALIGNER - (sizerequest % ALIGNER);
   }
 
 
@@ -227,14 +109,16 @@ void *malloc(size_t sizerequest){
   }
 
 
-  /*If we get here there wasn't a chunk large enough, create new one*/
+  /*If we get here there wasn't a chunk large enough, create new ones*/
   return more_memory_please(node, sizerequest);
 }
 
 
+
+
 int main(){
   char *str;
-  str = (char*) malloc(6);
+  str = (char*) myalloc(6);
 
   *str = 'h';
   *(str+1) = 'e';
@@ -243,47 +127,175 @@ int main(){
   *(str+4) = 'o';
   *(str+5) = '\0';
 
-  write(1, str, 6);
+  printf("str=%s, adr=%p\n", str, str);
+  fprintMemory();
 
   return 0;
 }
 
 
-// void free(void *ptr){
-//   if(ptr == NULL){
-//     return;
-//   }
-//
-//   /*
-//   each take a pointer to a block of memory allocated by malloc(3),
-//   but the pointer will not necessarily be to the very first byte of region.
-//   You must support this
-//
-//   You will have to remember to merge sections of memory if adjacent
-//   ones become free.
-//
-//   realloc(3) must try to minimize copying. That is, it must attempt
-//   in-place expansion (or shrinking) if it is possible, including merging
-//   with adjacent free chunks, if any
-//
-//   Also, remember, if realloc(3) is called with NULL it does the same
-//   thing as malloc(3), and if realloc(3) is called with a size of 0
-//   and the pointer is not NULL, it’s equivalent to free(ptr).2
-//   */
-// }
-//
-//
-// void *realloc(void *ptr, size_t sizerequest){
-//   if(ptr == NULL){
-//     return malloc(sizerequest);
-//   }
-//   else if(sizerequest == 0){
-//     free(ptr);
-//     return NULL;
-//   }
-//
-// }
-//
-// void *calloc(size_t nmemb, size_t sizerequest){
-//
-// }
+
+/* Clear and Allocate memory,
+  n=number of items
+  sz=size of item
+  Does the same as malloc except clearing all memory cells before return */
+void *calloc(size_t n, size_t sz){
+  size_t membytes = n*sz;
+  void *vp;
+
+  /*Memory couldn't be allocated*/
+  if((vp = myalloc(membytes)) == NULL){
+    return NULL;
+  }
+
+
+  /*clear all memory cells*/
+  long *tp = (long*) vp;
+  for(int i = 0; i<(membytes/sizeof(long)); i++){
+    *(tp+i) = 0;
+  }
+
+  return vp;
+}
+
+
+
+struct chunk *getchunk(uintptr_t address){
+  struct chunk *temp = MEM;
+  /*Checking if address is within previously allocated memory*/
+  if(address < temp->memptr || address > UPPERLIM)
+    return NULL; /*if not we return null*/
+
+  for(; temp->next; temp=temp->next){
+    if( (address >= temp->memptr) && (address < temp->next->memptr) ){
+      /*Address is between two adjacent nodes, belongs to first of them*/
+      return temp;
+    }
+  }
+  /*we're on last node*/
+  if((address >= temp->memptr) && (address <= UPPERLIM)){
+    return temp;
+  }
+  /*We can't end up here*/
+  printf("I misspoke...\n");
+}
+
+
+
+void free(void *ptr){
+  if(ptr == NULL){
+    return;
+  }
+
+  /*Get which chunk/header from pointer*/
+  struct chunk *c = getchunk( (uintptr_t) ptr);
+
+  if(c == NULL){
+    return; /*Memory could not be found in allocation*/
+  }
+
+  /*Mark chunk as free*/
+  c->isfree = TRUE;
+
+  /*IF adjacent chunk is free, include it?*/
+  //mergechunks();
+
+  return;
+
+  /*
+  each take a pointer to a block of memory allocated by malloc(3),
+  but the pointer will not necessarily be to the very first byte of region.
+  You must support this
+
+  You will have to remember to merge sections of memory if adjacent
+  ones become free.
+
+  realloc(3) must try to minimize copying. That is, it must attempt
+  in-place expansion (or shrinking) if it is possible, including merging
+  with adjacent free chunks, if any
+
+  Also, remember, if realloc(3) is called with NULL it does the same
+  thing as malloc(3), and if realloc(3) is called with a size of 0
+  and the pointer is not NULL, it’s equivalent to free(ptr)
+  */
+}
+
+
+
+
+
+void *realloc(void *ptr, size_t sizerequest){
+  if(ptr == NULL){
+    return myalloc(sizerequest);
+  }
+  else if(sizerequest == 0){
+    free(ptr);
+    return NULL;
+  }
+
+  /*Get which chunk/header from pointer*/
+  struct chunk *c = getchunk( (uintptr_t) ptr);
+
+  if(c == NULL){
+    return NULL; /*Memory could not be found in allocation*/
+  }
+
+  /*If we can fit memory in current chunk, just return the pointer*/
+  if(c->chunksize > sizerequest){
+    /*Just making sure address was allocated by malloc/calloc/realloc*/
+    if(!(c->isfree)){
+      return (void*) c->memptr;
+    }
+    else{/*memory was not allocated by malloc!*/
+      return NULL;
+    }
+  }
+  else if(c->next && c->next->isfree){
+
+    /*If next node is free we might be able to merge to get enough memory*/
+    uintptr_t accsize = c->chunksize;
+    struct chunk *t = c->next;
+
+    /*merging for as long as possible*/
+    while(t->next && t->next->isfree){
+      accsize += t->chunksize;
+
+      /*clear memory in between nodes and expand memory of original chunk*/
+      c->next = t->next;
+      c->chunksize = accsize;
+
+      if(accsize >= sizerequest){
+        /*Bingo, merged so that adjacent cells are free*/
+        /*Return original chunk now expanded*/
+        return (void *) c->memptr;
+      }
+      t = t->next;
+    }
+    /*We are on final node which still might work*/
+    if(t->isfree){
+      accsize += t->chunksize;
+      c->next = t;
+      c->chunksize = accsize;
+
+      if(accsize >= sizerequest){
+        return (void *) c->memptr;
+      }
+    }
+  }
+
+  /*Sadly chunk was not big enough nor expandable enough, must reallocate*/
+  void *vp = myalloc(sizerequest);
+  if(vp == NULL){
+    /*Could not allocate memory!*/
+    return NULL;
+  }
+  /*define pointers for copying*/
+  long *copy = (long *) c->memptr;
+  long *paste = (long *) vp;
+
+  for(int i = 0; i < (sizerequest/sizeof(long)); i++){
+    *(paste+i) = *(copy+i);
+  }
+  /*vp now contains original memory*/
+  return vp;
+}
